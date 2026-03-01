b7e74e525067ef0f693613297fe2306bdf867e11
youcode safi <tajat.nourelhouda@gmail.com>
2026-03-01 11:52:07 +0000

    feat: creation model Settlement

--- app/Models/Settlement.php
+++ app/Models/Settlement.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Settlement extends Model
 {
+    protected $fillable = [
+        'coloc_id', 
+        'debtor_id', 
+        'creditor_id', 
+        'amount', 
+        'date', 
+        'status'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'amount' => 'decimal:2',
+            'date' => 'date',
+        ];
+    }
+
+    public function colocation() {
+        return $this->belongsTo(Colocation::class, 'coloc_id'); 
+    }
+
+    public function debtor() { 
+        return $this->belongsTo(User::class, 'debtor_id'); 
+    }
     
+    public function creditor() { 
+        return $this->belongsTo(User::class, 'creditor_id'); 
+    }
 }
